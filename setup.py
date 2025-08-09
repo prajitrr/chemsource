e8fe7f3068d702d2efcba35281fc79e9d8c16217
prajitrr <prajkumar@ucsd.edu>
2025-08-09 00:20:14 -0700

    Added pyproject file

--- setup.py
+++ setup.py
@@ -1,37 +1,4 @@
-from setuptools import setup, find_packages
-from pathlib import Path
-import os
-import sys
+from setuptools import setup
 
-# Add src to path to import version
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
-from chemsource import __version__
-
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
-setup(
-    name="chemsource",
-    version=__version__,
-    author="Prajit Rajkumar",
-    author_email="prajkumar@ucsd.edu",
-    description="Tool to classify novel drugs and other health-related" 
-                + " chemicals by origin",
-    package_dir={"": "src"},
-    packages=find_packages(where='src'),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    install_requires=[
-        "lxml>=4.9.4",
-        "openai>=1.23.2",
-        "pyspellchecker>=0.8.1",
-        "requests>=2.0.0,<3",
-        "wikipedia>=1.4.0",
-    ],
-    long_description=long_description,
-    long_description_content_type='text/markdown'
-)
+# Configuration is now in pyproject.toml
+setup()
